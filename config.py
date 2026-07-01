aa36a3361b8878349c8f36f4dd70e6587c5af5e1
oid85 <oid85@mail.ru>
2026-07-02 02:00:24 +0700

    .

---
+++ config.py
@@ -0,0 +1,7 @@
+deploy_path = 'C:\\Deploy'
+
+algo_source_path = 'C:\\Repository\\Oid85.FinMarket.Algo\\Oid85.FinMarket.Algo'
+algo_deploy_directory = 'Oid85.FinMarket.Algo'
+algo_exe_file_name = "Oid85.FinMarket.Algo.WebHost.exe"
+algo_service_name = 'Oid85.FinMarket.Algo'
+algo_appsettings_file = 'appsettings.json'
